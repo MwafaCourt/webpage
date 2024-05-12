46c0fde12436bcdfa7a9af5830d2635bad3e14a7
MwafaCourt <169171345+MwafaCourt@users.noreply.github.com>
2024-05-12 22:29:33 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -27,7 +27,7 @@ $('img').ready(
         function() {
             $('.loader').addClass('hidden');
             $('.content').removeClass('hidden');
-        }, 8000
+        }, 3000
        )
     }
 )
